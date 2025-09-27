d1a0c29f89528ae825b3ac3d8dd0e1cf79b14054
Parameswari99 <eshuselvaraj626@gmail.com>
2025-09-27 16:49:49 +0530

    updated file

---
+++ src/Components/Hero2.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+
+const Hero2 = () => {
+    return (
+        <div class="slick-slider slick-initialized bg-slate-400" dir="ltr">
+            {/* <button type="button" data-role="none" class="slick-arrow slick-prev" style={{ display: "block" }}> Previous</button> */}
+            <div class="flex slick-list">
+                <div class="flex gap-8 items-center text-center min-w-max animate-slide lg:justify-around slick-track" style={{ width: "1730px", opacity: 1, transform: "translate3d('-207.6px', ' 0px', '0px') " }}>
+                    <div data-index="0" class="slick-slide" tabindex="-1" aria-hidden="true" style={{ outline: "none", width: "346px;" }}>
+                        <div>
+                            <div class="p-6 lg:p-12" tabindex="-1" style={{ width: "100 %", display: "inline - block" }}>
+                                <div class="w-60 lg:w-80 lg:h-full overflow-hidden relative bg-white shadow-lg rounded-xl p-3 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                    <img class="h-60 lg:h-80 rounded-md" alt="iPhone 17 Pro" src="./images/iphone_air.jpg"></img>
+                                    <p class="text-xs lg:text-sm font-medium text-[#b64429] mt-2">New</p>
+                                    <h2 class="text-lg lg:text-xl font-bold text-[#1d1d1f]">iPhone 17 Pro</h2>
+                                    <p class="text-xs lg:text-sm text-gray-500">Innovative design for ultimate performance and battery life.</p>
+                                    <div class="flex items-center space-x-2 mt-2">
+                                        <button class="text-xs lg:text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-3xl cursor-pointer">Learn more</button>
+                                        <p class="text-xs lg:text-sm text-blue-600 hover:underline cursor-pointer">Pre-Order ›</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-index="1" class="slick-slide slick-active slick-current" tabindex="-1" aria-hidden="false" style={{ outline: "none", width: "346px;" }}>
+                        <div>
+                            <div class="p-6 lg:p-12" tabindex="-1" style={{ width: "100%", display: "inline - block" }}>
+                                <div class="w-60 lg:w-80 lg:h-full overflow-hidden relative bg-white shadow-lg rounded-xl p-3 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                    <img class="h-60 lg:h-80 rounded-md" alt="iPhone Air" src="./images/iphone_seventeen.jpg"></img>
+                                    <p class="text-xs lg:text-sm font-medium text-[#b64429] mt-2">New</p>
+                                    <h2 class="text-lg lg:text-xl font-bold text-[#1d1d1f]">iPhone Air</h2>
+                                    <p class="text-xs lg:text-sm text-gray-500">The thinnest iPhone ever.With the power of pro inside.</p>
+                                    <div class="flex items-center space-x-2 mt-2">
+                                        <button class="text-xs lg:text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-3xl cursor-pointer">Learn more</button>
+                                        <p class="text-xs lg:text-sm text-blue-600 hover:underline cursor-pointer">Pre-Order ›
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-index="2" class="slick-slide slick-active" tabindex="-1" aria-hidden="false" style={{ outline: "none", width: "346px;" }}>
+                        <div>
+                            <div class="p-6 lg:p-12" tabindex="-1" style={{ width: "100%", display: "inline - block" }}>
+                                <div class="w-60 lg:w-80 lg:h-full overflow-hidden relative bg-white shadow-lg rounded-xl p-3 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                    <img class="h-60 lg:h-80 rounded-md" alt="iPhone 17" src="./images/iphone_sixteen_e.jpg"></img>
+                                    <p class="text-xs lg:text-sm font-medium text-[#b64429] mt-2">New</p>
+                                    <h2 class="text-lg lg:text-xl font-bold text-[#1d1d1f]">iPhone 17</h2>
+                                    <p class="text-xs lg:text-sm text-gray-500">Innovative design for ultimate performance and battery life.</p>
+                                    <div class="flex items-center space-x-2 mt-2">
+                                        <button class="text-xs lg:text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-3xl cursor-pointer">Learn more</button>
+                                        <p class="text-xs lg:text-sm text-blue-600 hover:underline cursor-pointer">Pre-Order ›</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-index="3" class="slick-slide slick-active" tabindex="-1" aria-hidden="false" style={{ outline: "none", width: "346px" }}>
+                        <div>
+                            <div class="p-6 lg:p-12" tabindex="-1" style={{ width: "100%", display: "inline - block" }}>
+                                <div class="w-60 lg:w-80 lg:h-full overflow-hidden relative bg-white shadow-lg rounded-xl p-3 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                    <img class="h-60 lg:h-80 rounded-md" alt="iPhone 16" src="./images/iphone_sixteen.jpg"></img>
+                                    <p class="text-xs lg:text-sm font-medium text-[#b64429] mt-2">Best Seller</p>
+                                    <h2 class="text-lg lg:text-xl font-bold text-[#1d1d1f]">iPhone 16</h2>
+                                    <p class="text-xs lg:text-sm text-gray-500">Amazing performance.Durable design.Make it yours.</p>
+                                    <div class="flex items-center space-x-2 mt-2">
+                                        <button class="text-xs lg:text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-3xl cursor-pointer">Learn more</button>
+                                        <p class="text-xs lg:text-sm text-blue-600 hover:underline cursor-pointer">Buy ›</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-index="4" class="slick-slide slick-active" tabindex="-1" aria-hidden="false" style={{ outline: "none", width: "346px" }}><div>
+                        <div class="p-6 lg:p-12" tabindex="-1" style={{ width: "100%", display: "inline - block" }}>
+                            <div class="w-60 lg:w-80 lg:h-full overflow-hidden relative bg-white shadow-lg rounded-xl p-3 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                                <img class="h-60 lg:h-80 rounded-md" alt="iPhone 16e" src="./images/iphoneseventeenp.jpg"></img>
+                                <p class="text-xs lg:text-sm font-medium text-[#b64429] mt-2">Compact</p><h2 class="text-lg lg:text-xl font-bold text-[#1d1d1f]">iPhone 16e</h2>
+                                <p class="text-xs lg:text-sm text-gray-500">Everything you love.Including the price.Cutting-Edge Cameras.</p>
+                                <div class="flex items-center space-x-2 mt-2">
+                                    <button class="text-xs lg:text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-3xl cursor-pointer">Learn more</button>
+                                    <p class="text-xs lg:text-sm text-blue-600 hover:underline cursor-pointer">Buy ›</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/* <button type="button" data-role="none" class="slick-arrow slick-next slick-disabled" style={{ display: "block" }}> Next</button>
+            <ul class="slick-dots" style={{ display: "block" }}>
+                <li class="">
+                    <button>1</button>
+                </li>
+                <li class="">
+                    <button>2</button>
+                </li>
+            </ul> */}
+        </div>
+    )
+}
+
+export default Hero2
